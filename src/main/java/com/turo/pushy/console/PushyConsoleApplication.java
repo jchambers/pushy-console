349395904dad8db3ddbfb8bcd38c323c8426cde3
Jon Chambers <jon.chambers@gmail.com>
2018-05-07 23:27:07 -0400

    Optimize imports.

--- src/main/java/com/turo/pushy/console/PushyConsoleApplication.java
+++ src/main/java/com/turo/pushy/console/PushyConsoleApplication.java
@@ -6,8 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.InputStream;
-import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class PushyConsoleApplication extends Application {
